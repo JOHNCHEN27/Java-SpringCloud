885172d8394ee6167f01fc96eea89454b93eea11
JOHNCHEN27 <2502523383@qq.com>
2023-08-29 20:37:23 +0800

    初识Sentinel

--- CloudBase/feign-api/src/main/java/com/lncanswer/clients/UserClient.java
+++ CloudBase/feign-api/src/main/java/com/lncanswer/clients/UserClient.java
@@ -1,12 +1,17 @@
 package com.lncanswer.clients;
 
+import com.lncanswer.factory.UserClientFallbackFactory;
 import com.lncanswer.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient("userservice") //绑定服务 加上FeignClient注解spring会自动扫描此配置类
+/**
+ *在接口中使用定义的UserClientFallbackFactory类
+ */
+@FeignClient(value = "userservice",fallbackFactory = UserClientFallbackFactory.class)
+//绑定服务 加上FeignClient注解spring会自动扫描此配置类
 public interface UserClient {
 
     //编写请求方法:请求方式、参数、返回值 方法名
